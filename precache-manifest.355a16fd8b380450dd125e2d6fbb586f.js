82568c473743ed964b8c41ea35e53d51b7a1c68a
OliverKK <oliver.kinderknecht@googlemail.com>
2020-12-29 16:51:46 +0100

    Updates

---
+++ precache-manifest.355a16fd8b380450dd125e2d6fbb586f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9a80c084be94b71581326aa3097cb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8aaf5af9f5b04437b4f",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "30cbeadedf855d5f72d7",
+    "url": "./static/js/2.60f6de9e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.60f6de9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8aaf5af9f5b04437b4f",
+    "url": "./static/js/main.e332c256.chunk.js"
+  },
+  {
+    "revision": "5cdc9ab30143e6c6caa4",
+    "url": "./static/js/runtime-main.ee4ff590.js"
+  }
+]);
