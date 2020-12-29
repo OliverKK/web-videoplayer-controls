9f7382150fac7f844caa30c4629ef57daece2885
OliverKK <oliver.kinderknecht@googlemail.com>
2020-12-29 16:50:32 +0100

    [TASK] Add default video player

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'web-videoplayer-controls'
 import 'web-videoplayer-controls/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,25 @@ import * as React from 'react'
 import styles from './styles.module.css'
 
 interface Props {
-  text: string
+  title?: string,
+  videos?: Array<string>,    // List of urls to videos  
+  width?: number|string,     // 40 or '40px'
+  height?: number|string,    // 40 or '40px'
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Exsssssmple Component: {text}</div>
+export const ExampleComponent = ({ 
+  title = 'Web Video Player with custom contols 😄',
+  width = '100%',
+  height = '350px',
+}: Props) => {
+  return (
+    <div className={styles.test}>
+      <h5>{title}</h5>
+      <video controls width={width} height={height} muted>
+        <source src="https://www.odt.net/static/media/Skills-teaser.e85b7707.webm" type="video/webm" />
+        <source src="https://www.odt.net/static/media/Skills-teaser.e85b7707.mp4" type="video/mp4" />
+        This browser does not support the HTML5 video element.
+      </video>
+    </div>
+  )
 }
