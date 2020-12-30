33312a1d72abe52c371f232f189dbe8235ce9555
OliverKK <oliver.kinderknecht@googlemail.com>
2020-12-30 16:37:12 +0100

    [TASK] Remove bad interval approach

--- src/index.tsx
+++ src/index.tsx
@@ -20,37 +20,14 @@ export const ExampleComponent = ({
   controls = false
 }: Props) => {
 
-  let interval:any = undefined;
-
   const videoRef:React.RefObject<any> = React.createRef();
-  const canvasRef:React.RefObject<any> = React.createRef();
+  const progressBarRef:React.RefObject<any> = React.createRef();
 
-  const videoStatusCheckPeriodically = () => {
-    const videoElement:HTMLVideoElement = videoRef.current;
-
-    interval = setInterval(() => {
-      const { currentTime, duration } = videoElement;
-      const currentPercentage = getPercentageOfVideoAlreadyPlayed(currentTime, duration);
-      
-      draw(currentPercentage);
-    }, 50);
-  }
-
-  const videoStatusCheckPeriodicallyStop = () => {
-    clearInterval(interval);
-  }
-
-  const handlePlay = () => {     
-    videoStatusCheckPeriodically();
-  };
-
-  const handlePause = () => {
-    videoStatusCheckPeriodicallyStop();
-    if(interval) clearInterval(interval);
-  };
-
-  const handleEnded = () => {     
-    videoStatusCheckPeriodicallyStop();
+  const handleTimeUpdate = () => {   
+    const videoElement:HTMLVideoElement = videoRef.current;  
+    const { currentTime, duration } = videoElement;
+    const currentPercentage = getPercentageOfVideoAlreadyPlayed(currentTime, duration);
+    progressBarRef.current.style.width = `${currentPercentage}%`;      
   };
 
   const getPercentageOfVideoAlreadyPlayed = (current:number, duration:number) => {
@@ -76,41 +53,15 @@ export const ExampleComponent = ({
     if (!currentVidRef) return;
     currentVidRef.pause();
     currentVidRef.currentTime = 0;
-    resetCanvas();
-  }
-
-  const draw = (percentage:number) => {
-    const canvas = canvasRef.current;
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-      const width = canvas.width;
-      const widthByPercentage = width * percentage / 100;
-      const height = canvas.height;
-
-      ctx.fillStyle = "rgb(200,0,0)";
-      ctx.fillRect(0, 0, widthByPercentage, height);
-    } 
-  }
-
-  const resetCanvas = () => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
   }
 
   useEffect(() => {
     const currentVideoRef:HTMLVideoElement = videoRef.current;
 
-    currentVideoRef.addEventListener('play', handlePlay, true);
-    currentVideoRef.addEventListener('pause', handlePause, true);
-    currentVideoRef.addEventListener('ended', handleEnded, true);
+    currentVideoRef.addEventListener('timeupdate', handleTimeUpdate, true);
 
     return () => {
-      currentVideoRef.removeEventListener('play', handlePlay, true);
-      currentVideoRef.removeEventListener('pause', handlePause, true);
-      currentVideoRef.removeEventListener('ended', handleEnded, true);
-
-      if(interval) clearInterval(interval);
+      currentVideoRef.removeEventListener('timeupdate', handleTimeUpdate, true);
     }
   }, [])
   
@@ -133,7 +84,9 @@ export const ExampleComponent = ({
         }
         This browser does not support the HTML5 video element.
       </video>
-      <canvas ref={canvasRef} id="wcv-progress-bar" className={styles.wvcProgressBar}></canvas>
+      <div className={styles.wvcProgressBarWrapper}>
+        <div ref={progressBarRef} className={styles.wvcProgressBar} />
+      </div>      
       <button onClick={() => handlePlayControl(videoRef.current)}>play</button>
       <button onClick={() => handlePauseControl(videoRef.current)}>pause</button>
       <button onClick={() => handleStopControl(videoRef.current)}>stop/reset</button>
